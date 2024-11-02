dfd5455be7b0735fe7c4cd777fa5192df43cbd65
Lucas Rodrigues dos Santos <45694838+llucasy@users.noreply.github.com>
2024-11-02 19:54:22 +0000

    feat: stylization of the selected answer, correct and incorrect

--- src/lib/components/Option.svelte
+++ src/lib/components/Option.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
   import { quizStore } from '$lib/stores/gameStore'
 
-  const { option, selectOption, answer }: { option: string; selectOption: () => void; answer: string } = $props()
+  const {
+    option,
+    selectOption,
+    answer,
+  }: { option: string; selectOption: () => void; answer: string } = $props()
 
-  const correctCSS = '!bg-[#0bfc03] font-bold'
-  const wrongCSS = 'bg-[#3c0e70] opacity-100'
+  const correctCSS = '!bg-green-500 '
+  const selectedCSS = '!border-4 !border-white font-bold '
 </script>
 
 <button
   type="button"
-  onclick={() => selectOption()}
-  class={'!mb-4 !transform !cursor-pointer !rounded-lg !bg-[#3c0e70] !p-4 !opacity-80 !transition duration-300 ease-in-out hover:scale-110 hover:opacity-100 ' +
+  onclick={() => {
+    selectOption()
+  }}
+  class={'!mb-4 !transform !cursor-pointer !rounded-lg !bg-[#3c0e70] !p-4 !opacity-80 !transition duration-300 ease-in-out hover:scale-105 hover:border-4 hover:shadow-2xl hover:shadow-green-500/50 ' +
     ($quizStore.answerSelected && option === answer ? correctCSS : '') +
-    ($quizStore.answerSelected && option !== answer ? wrongCSS : '')}
+    ($quizStore.answerSelected === option ? selectedCSS : '') +
+    ($quizStore.answerSelected === option && $quizStore.answerSelected !== answer
+      ? '!bg-red-500'
+      : '')}
 >
   <p>{option}</p>
 </button>
